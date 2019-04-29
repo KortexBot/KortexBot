7b9cb77e9d8ea5fb05d24ac99d7a14eddf048656
KortexBot <50108825+KortexBot@users.noreply.github.com>
2019-04-29 15:42:11 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+bot.on('ready', function () {
+	bot.user.setPresence({ game: { name: 'Aide Mewthree à gerer le serveur!', type: "streaming", url: "https://www.twitch.tv/kortexbot"}});
+  console.log("Connecte")
+});
+
+bot.on("guildMemberAdd", member => {
+ 	member.guild.channels.find("id", "570851457016922122").send(`Bienvenue ${member.user.name} sur le serveur de la Kortex !`)
+ });
+
+
+
+
+
+
+
+
+
+
+bot.login('NTcxMzY0MjAwMjQ5Njg4MTAz.XMMq8g.mVY0S3mZg_TbyFIGCoLKr7XZPDM');
+
